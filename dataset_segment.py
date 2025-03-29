b9fae16cadf244c309dfd0dfb5156e61a0b9e60e
boyiwei <wby@princeton.edu>
2025-03-29 14:37:17 +0000

    update subset info

---
+++ dataset_segment.py
@@ -0,0 +1,22 @@
+import json
+import math
+
+# Load JSON data from file
+
+json_name = "Intercode_CTF/intercode_dataset"
+with open(f"{json_name}.json", 'r') as f:
+    data = json.load(f)
+
+# Assuming data is a list, determine the size of each segment
+n = len(data)
+segment_size = math.ceil(n / 5)
+keys = list(data.keys())
+# Split and save each segment
+num_segments = (len(keys) + segment_size - 1) // segment_size
+
+# Split and save each segment
+for i in range(num_segments):
+    segment_keys = keys[i * segment_size:(i + 1) * segment_size]
+    segment = {k: data[k] for k in segment_keys}
+    with open(f'{json_name}{i+1}.json', 'w') as out_file:
+        json.dump(segment, out_file, indent=2)
