8ac21209541f3a94b43e10240c3461d275e1fa34
boyiwei <wby@princeton.edu>
2025-04-03 15:55:50 +0000

    update segment

--- dataset_segment.py
+++ dataset_segment.py
@@ -9,7 +9,7 @@ with open(f"{json_name}.json", 'r') as f:
 
 # Assuming data is a list, determine the size of each segment
 n = len(data)
-segment_size = math.ceil(n / 5)
+segment_size = math.ceil(n /5)
 keys = list(data.keys())
 # Split and save each segment
 num_segments = (len(keys) + segment_size - 1) // segment_size
@@ -18,5 +18,5 @@ num_segments = (len(keys) + segment_size - 1) // segment_size
 for i in range(num_segments):
     segment_keys = keys[i * segment_size:(i + 1) * segment_size]
     segment = {k: data[k] for k in segment_keys}
-    with open(f'{json_name}{i+1}.json', 'w') as out_file:
+    with open(f'{json_name}_segment{num_segments}_{i+1}.json', 'w') as out_file:
         json.dump(segment, out_file, indent=2)
